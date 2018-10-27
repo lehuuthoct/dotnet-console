6206ef82d1a1a32fda7d8c10477ef497a6f6147d
lehuuthoct <lehuuthoct@gmail.com>
2018-10-27 22:25:27 +1100

    * CsvReader.cs: add CsvReader
    * project.assets.json:
    * hwapp.csproj.nuget.cache:
    * hwapp.csproj.nuget.g.props:
    * hwapp.csprojAssemblyReference.cache:

---
+++ CsvReader.cs
@@ -0,0 +1,43 @@
+using System;
+using System.IO;
+using System.Collections.Generic;
+
+namespace hwapp
+{
+  public class CsvReader
+  {
+    private string[] columns;
+    private TextReader source;
+
+    public CsvReader(TextReader reader)
+    {
+      this.source = reader;
+      var columnLine = reader.ReadLine();
+      this.columns = columnLine.Split(',');
+    }
+
+    public IEnumerable<KeyValuePair<string, string>> Lines
+    {
+      get
+      {
+        string row;
+        while ((row = this.source.ReadLine()) != null)
+        {
+          var cells = row.Split(',');
+          var pairs = new KeyValuePair<string, string>[columns.Length];
+
+          for (int col = 0; col < columns.Length; col++)
+          {
+            pairs[col] = new KeyValuePair<string, string>(
+              columns[col], cells[col]
+            );
+          }
+
+          yield return pairs;
+
+        }
+      }
+    }
+
+  }
+}
